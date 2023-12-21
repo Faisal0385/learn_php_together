aea00c249e5c7f548806c571da695c56e94dfade
Faisal A. Salam <faisaltez@gmail.com>
2023-12-22 00:04:59 +0600

    Basic PHP Syntax

---
+++ 1_Basic_PHP_Syntax/Class_Code/page_1.php
@@ -0,0 +1,4 @@
+<?php
+//php code without html
+
+echo "Hi, This is a Php code!!";
---
+++ 1_Basic_PHP_Syntax/Class_Code/page_2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .text-color{
+            color: green;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    // writing php code inside a html page
+    echo "This is php from html page!!";
+
+    echo "<h1>This is php from html tag!!</h1>";
+    echo "<h1 class='text-color'>This is from with a css class!!</h1>";
+    echo "<h1 style='font-size:20px'>This is from with a css style!!</h1>";
+
+
+    ?>
+</body>
+
+</html>
