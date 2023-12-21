3f989d88b37a49deaca652b0f1fa524d79f221e1
Faisal A. Salam <faisaltez@gmail.com>
2023-12-22 00:25:15 +0600

    Php Case Sensitive and Comments

---
+++ 2_Php_Case_Sensitive_and_Comments/Class_Code/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+    Php is case-sensitive only to the variable we create,
+    but it is not case-sensetive to the php key words 
+*/
+
+echo "This is first line";
+eCHo "This is second line";
+ECHO "This is third line";
+
+$name = "Faisal";
+
+echo $name;
+// echo $Name;
+
+// this is single line comment
+
+/*
+    This is a multi-line comment
+    This is a multi-line comment
+    This is a multi-line comment
+*/
